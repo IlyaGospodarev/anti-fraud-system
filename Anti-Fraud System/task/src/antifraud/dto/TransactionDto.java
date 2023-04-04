9212901acb89ffbfa0b2864c22b9ef0d004a0c82
Ilya Gospodarev <gospodarev88@gmail.com>
2023-04-04 20:22:35 +0200

    Create response Transaction class.

--- Anti-Fraud System/task/src/antifraud/dto/TransactionDto.java
+++ Anti-Fraud System/task/src/antifraud/dto/TransactionDto.java
@@ -1,4 +1,20 @@
 package antifraud.dto;
 
 public class TransactionDto {
+    private String result;
+
+    public TransactionDto() {
+    }
+
+    public TransactionDto(String result) {
+        this.result = result;
+    }
+
+    public String getResult() {
+        return result;
+    }
+
+    public void setResult(String result) {
+        this.result = result;
+    }
 }
