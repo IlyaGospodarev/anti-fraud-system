963de891d3ff1454bf5f14c0f18827649cf9c67f
Ilya Gospodarev <gospodarev88@gmail.com>
2023-04-04 20:22:03 +0200

    Create Transaction controller with Post request.

--- Anti-Fraud System/task/src/antifraud/controller/TransactionController.java
+++ Anti-Fraud System/task/src/antifraud/controller/TransactionController.java
@@ -1,7 +1,25 @@
 package antifraud.controller;
 
+import antifraud.dto.TransactionDto;
+import antifraud.model.Transaction;
+import antifraud.service.TransactionService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/antifraud/transaction")
 public class TransactionController {
+    private final TransactionService transactionService;
+
+    public TransactionController(TransactionService transactionService) {
+        this.transactionService = transactionService;
+    }
+
+    @PostMapping
+    public ResponseEntity<TransactionDto> sendAmount(@RequestBody Transaction transaction) {
+        return ResponseEntity.ok(transactionService.sendAmount(transaction));
+    }
 }
