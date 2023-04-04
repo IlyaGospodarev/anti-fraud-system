ba4c015dda367926c8bf490dbeb8dc0331235b3d
Ilya Gospodarev <gospodarev88@gmail.com>
2023-04-04 20:24:28 +0200

    Create Transaction repository with one method, witch checks amount value.

--- Anti-Fraud System/task/src/antifraud/repository/TransactionRepository.java
+++ Anti-Fraud System/task/src/antifraud/repository/TransactionRepository.java
@@ -1,7 +1,31 @@
 package antifraud.repository;
 
+import antifraud.dto.TransactionDto;
+import antifraud.exception.InvalidTransactionAmount;
+import antifraud.model.Transaction;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class TransactionRepository {
+
+    public TransactionDto checkTransactionSum(Transaction transaction) {
+        int amount = transaction.getAmount();
+
+        TransactionDto transactionDto = new TransactionDto();
+
+        if (amount < 1) {
+            throw new InvalidTransactionAmount();
+        }
+
+        if (amount <= 200) {
+            transactionDto.setResult("ALLOWED");
+        } else if (amount <= 1500) {
+            transactionDto.setResult("MANUAL_PROCESSING");
+        } else {
+            transactionDto.setResult("PROHIBITED");
+        }
+
+        return transactionDto;
+    }
 }
+
