4afefe2d540af7a6d9f0bf642dd82556a2e55cfd
Ilya Gospodarev <gospodarev88@gmail.com>
2023-04-04 20:24:43 +0200

    Create Transaction service.

--- Anti-Fraud System/task/src/antifraud/service/TransactionService.java
+++ Anti-Fraud System/task/src/antifraud/service/TransactionService.java
@@ -1,7 +1,20 @@
 package antifraud.service;
 
+import antifraud.dto.TransactionDto;
+import antifraud.model.Transaction;
+import antifraud.repository.TransactionRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TransactionService {
+
+    private final TransactionRepository transactionRepository;
+
+    public TransactionService(TransactionRepository transactionRepository) {
+        this.transactionRepository = transactionRepository;
+    }
+
+    public TransactionDto sendAmount(Transaction transaction) {
+        return transactionRepository.checkTransactionSum(transaction);
+    }
 }
