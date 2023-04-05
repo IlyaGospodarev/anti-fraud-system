f44d685534f0b299f127522db8a99897f792d453
Ilya Gospodarev <gospodarev88@gmail.com>
2023-04-05 16:18:47 +0200

    Creating a class associated with changing the user's role.

---
+++ Anti-Fraud System/task/src/antifraud/dto/EditUserRoleRequest.java
@@ -0,0 +1,34 @@
+package antifraud.dto;
+
+import javax.validation.constraints.NotBlank;
+
+public class EditUserRoleRequest {
+    @NotBlank
+    private String username;
+    @NotBlank
+    private String role;
+
+    public EditUserRoleRequest() {
+    }
+
+    public EditUserRoleRequest(String username, String role) {
+        this.username = username;
+        this.role = role;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
